b7ae9652c8eb856e45576d84328c813a43892017
ZhuoranTian <849597043@qq.com>
2021-09-15 12:09:12 +0800

    语法练习

--- JavaPractice/变量类型.java
+++ JavaPractice/变量类型.java
@@ -7,14 +7,3 @@ public class 变量类型 {
         System.out.printf("%s", v1.str);
     }
 }
-class Variable {
-    int allClicks = 0;    // 类变量
-
-    String str = "hello world";  // 实例变量
-
-    public void method() {
-
-        int i = 0;  // 局部变量
-
-    }
-}
