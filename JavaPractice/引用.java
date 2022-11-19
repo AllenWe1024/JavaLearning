039dd5898966b2869c84f5ac163e8b0ac23c707d
AllenWe1024 <849597043@qq.com>
2022-11-19 16:56:41 +0800

    LeetCode数组

--- JavaPractice/引用.java
+++ JavaPractice/引用.java
@@ -3,7 +3,7 @@ public class 引用 {
         String s = "hello";
         String t = s;
         s = "world";
-        System.out.println("test_remote")
         System.out.println(t); // t是"hello"还是"world"?
+        System.out.println("test_local");
     }
 }
