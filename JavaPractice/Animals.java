e86a16c457d077e9686bd69fb7ad747e3abcf367
Your Name <you@example.com>
2022-11-19 16:49:16 +0800

    Review

---
+++ JavaPractice/Animals.java
@@ -0,0 +1,10 @@
+public class Animals{
+    public Puppy(String name){
+        //这个构造器仅有一个参数：name
+        System.out.println("小狗的名字是 : " + name );
+    }
+    public static void main(String[] args){//程序入口
+        // 下面的语句将创建一个Puppy对象
+        Animals myPuppy = new Animals( "tommy" );//注意与cpp区别
+    }
+}
