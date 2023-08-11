3f372387153a37f55df80d3ae4360a8aa7b8448d
AllenWe1024 <849597043@qq.com>
2023-08-11 14:31:30 +0800

    test

---
+++ JavaPractice/数组.java
@@ -0,0 +1,17 @@
+public class 数组 {
+    public static void main(String[] args) {
+        int[] x, y[];
+        // a)
+        x[0] = y; // no
+        // b)
+        y[0] = x; // yes
+        // c)
+        y[0][0] = x; // no
+        // d)
+        x[0][0] = y; // no
+        // e)
+        y[0][0] = x[0]; // yes
+        // f)
+        x = y; // no
+    }
+}
